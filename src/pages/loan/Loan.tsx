e432ee102c7a579730c93a43e8255cbe6c15d04e
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-12-15 21:47:55 +0700

    reformat loan code

--- src/pages/loan/Loan.tsx
+++ src/pages/loan/Loan.tsx
@@ -3,19 +3,16 @@ import { Link, useSearchParams } from "react-router";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import PageMeta from "../../components/common/PageMeta";
-import Select from "../../components/form/Select";
-import DatePicker from "../../components/form/date-picker";
-import Button from "../../components/ui/button/Button";
-import { Dropdown } from "../../components/ui/dropdown/Dropdown";
-import { useTheme } from "../../context/ThemeContext";
-import axios from "../../utils/axios";
-import { GroupProps, LoanProps, PaginationProps, ScheduleProps } from "../../utils/types";
-import Table from "./LoanTable";
 import SelectSearch from "../../components/form/SelectSearch";
-import { toLocalDate } from "../../utils/helpers";
-import { SearchIcon } from "../../icons";
 import Input from "../../components/form/input/InputField";
 import Loading from "../../components/ui/Loading";
+import Button from "../../components/ui/button/Button";
+
+import { useTheme } from "../../context/ThemeContext";
+import { SearchIcon } from "../../icons";
+import { useLoan } from './hooks/useLoan';
+import Filter from './components/Filter';
+import Table from './LoanTable';
 
 const days = [
     'all', "senin", "selasa", "rabu", "kamis", "jumat", "sabtu"
@@ -31,83 +28,29 @@ const dayMap: any = {
 };
 
 const Loan: React.FC = () => {
-    const [filter, setFilter] = useState<{ startDate: String | null; endDate: String | null; status: string | null }>({
-        startDate: null,
-        endDate: null,
-        status: null
-    });
-    const [loans, setLoans] = useState<LoanProps[]>([]);
-    const [dayFilter, setDayFilter] = useState("all");
-    const [groupFilter, setGroupFilter] = useState("all");
-    const [groups, setGroups] = useState<{ label: string, value: string }[]>([]);
-    const [search, setSearch] = useState<String | null>("");
-    const [isFiltersLoaded, setIsFiltersLoaded] = useState(false);
+    const {
+        loans,
+        groups,
+        pagination,
+        setPagination,
+        filter,
+        setFilter,
+        dayFilter,
+        setDayFilter,
+        groupFilter,
+        setGroupFilter,
+        search,
+        setSearch,
+        isFiltersLoaded,
+        fetchLoans,
+    } = useLoan();
     const [searchParams] = useSearchParams();
-
-    const [pagination, setPagination] = useState<PaginationProps>({
-        page: 1,
-        totalPages: 1,
-        limit: 10,
-        total: 0
-    });
     const { reload } = useTheme();
 
     useEffect(() => {
-        const stored = localStorage.getItem('filters');
-        console.log(stored);
-        axios
-            .get(`/api/loans?page=${pagination?.page}&status=${filter.status}&startDate=${filter.startDate}&endDate=${filter.endDate}&day=${dayMap[dayFilter]}&group=${groupFilter}&search=${search}`)
-            .then((res: any) => {
-                setLoans(res.data.loans);
-                setPagination((prev) => ({ ...prev, totalPages: res.data.pagination.totalPages }));
-            });
-        axios
-            .get(`/api/groups?limit=20000000`)
-            .then((res: any) => {
-                const { groups } = res.data
-                // let scheduleFilter = schedule.filter((s: any) => dayFilter == "all" || s.day == dayFilter);
-                // setGroups(scheduleFilter.map((group: ScheduleProps) => ({ label: group.group.group_name + " | " + group.day, value: group.group.id })))
-                setGroups(
-                    groups
-                        .slice() // copy array biar tidak mutate state asli
-                        .sort((a: GroupProps, b: GroupProps) =>
-                            a.group_name.localeCompare(b.group_name)
-                        )
-                        .map((group: GroupProps) => ({
-                            label: group.group_name,
-                            value: group.id,
-                        }))
-                );
-            });
+        fetchLoans(pagination.page);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [pagination.page, reload, filter.endDate, filter.startDate, filter.status, dayFilter, groupFilter, search]);
-
-
-    useEffect(() => {
-        const stored = localStorage.getItem('filters');
-        // const isFromTransaction = localStorage.getItem('statusTransaction');
-        // perlu perbaikan
-
-        const isFromTransaction = searchParams.get("isFromTransaction");
-        // perlu perbaikan
-
-
-        if (stored && isFromTransaction) {
-            const savedFilters = JSON.parse(stored);
-            setFilter((prev: any) => ({
-                ...prev,
-                endDate: savedFilters.endDate || '',
-                startDate: savedFilters.startDate || '',
-                status: savedFilters.status || '',
-            }));
-
-            setDayFilter(savedFilters.dayFilter || 'all');
-            setGroupFilter(savedFilters.groupFilter || '');
-            setSearch(savedFilters.search || '');
-            setPagination((prev) => ({ ...prev, page: savedFilters.page }));
-
-        }
-        setIsFiltersLoaded(true);
-    }, []);
 
 
     useEffect(() => {
@@ -217,105 +160,6 @@ const Loan: React.FC = () => {
                 </ComponentCard>
             </div>
         </>
-    );
-};
-
-interface FilterProps {
-    filter: {
-        startDate: String | null;
-        endDate: String | null;
-        status: string | null;
-    };
-    setFilter: (filter: { startDate: String | null; endDate: String | null; status: string | null }) => void;
-}
-const Filter: React.FC<FilterProps> = ({ filter, setFilter }) => {
-    const [isOpenDropdown, setIsOpenDropdown] = useState(false);
-    const [startDate, setStartDate] = useState<String | null>(filter.startDate);
-    const [endDate, setEndDate] = useState<String | null>(filter.endDate);
-    const [status, setStatus] = useState<string | null>(filter.status);
-
-    useEffect(() => {
-        setStartDate(filter.startDate)
-        setEndDate(filter.endDate)
-        setStatus(filter.status)
-    }, [filter.startDate, filter.endDate, filter.status]);
-    const openDropdown = () => setIsOpenDropdown(true);
-    const closeDropdown = () => setIsOpenDropdown(false);
-
-    const handleFilter = () => {
-        setFilter({ startDate, endDate, status });
-        closeDropdown();
-    };
-    return (
-        <div className="relative">
-            <Button
-                size="sm"
-                variant="outline"
-                onClick={openDropdown}
-                className="flex items-center text-gray-700 dark:text-gray-400 px-4"
-            >
-                Filter
-            </Button>
-
-            <Dropdown
-                isOpen={isOpenDropdown}
-                onClose={closeDropdown}
-                className="absolute right-0 mt-[17px] w-[600px] rounded-2xl border border-gray-200 bg-white p-4 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark"
-            >
-                <div className="flex flex-col gap-3 text-sm text-gray-700 dark:text-gray-300">
-                    <div>
-                        <label className="block mb-1 font-medium">Tanggal Peminjaman</label>
-                        <DatePicker
-                            hasClear
-                            id={"startDate"}
-                            mode="range"
-                            placeholder="Tanggal peminjaman"
-                            defaultDate={
-                                startDate && endDate
-                                    ? [new Date(startDate as string), new Date(endDate as string)]
-                                    : (startDate ? [new Date(startDate as string)] : undefined)
-                            }
-                            onChange={(date) => {
-                                date[0]
-                                    ? setStartDate(toLocalDate(date[0]))
-                                    : (setStartDate(null));
-
-
-                                date[1]
-                                    ? setEndDate(toLocalDate(date[1]))
-                                    : setEndDate(null);
-                            }}
-                        />
-                    </div>
-
-                    <div>
-                        <label className="block mb-1 font-medium">Status Peminjaman</label>
-                        <Select
-                            placeholder="Pilih status"
-                            options={[
-                                { label: "Semua", value: "" },
-                                { label: "Aktif", value: "aktif" },
-                                { label: "Menunggak", value: "menunggak" },
-                                { label: "Lunas", value: "lunas" }
-                            ]}
-                            className="w-full rounded-md border px-2 py-1 dark:bg-gray-800 dark:border-gray-700"
-                            value={status ?? ""}
-                            defaultValue={filter.status ?? ""}
-                            onChange={(e) => setStatus(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="flex justify-end gap-2 mt-2">
-                        <Button size="sm" variant="outline" onClick={closeDropdown}>
-                            Batal
-                        </Button>
-                        <Button size="sm" onClick={handleFilter}>
-                            Terapkan
-                        </Button>
-                    </div>
-                </div>
-            </Dropdown>
-        </div>
     );
 };
 
