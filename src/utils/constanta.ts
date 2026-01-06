ebe3d3cd7929e6426e12cb5644be95d854817a86
Kholzt <99956928+Kholzt@users.noreply.github.com>
2026-01-06 09:36:04 +0700

    Update constanta.ts

--- src/utils/constanta.ts
+++ src/utils/constanta.ts
@@ -3,6 +3,12 @@ export interface PosisiUsahaMap {
   readonly STORTING: string;
   readonly TARGET: string;
   readonly TARGET_ANGGOTA: string;
+  readonly IP: string;
+  readonly SIRKULASI: string;
+  readonly SIRKULASI_JALAN: string;
+  readonly SU: string;
+  readonly PD: string;
+  readonly NAIK_TURUN: string;
 }
 
 // 2. Implementasikan dengan 'as const' untuk keamanan maksimal
@@ -11,6 +17,12 @@ const posisiUsahaCode: PosisiUsahaMap = {
   STORTING: "storting",
   TARGET: "target",
   TARGET_ANGGOTA: "targetanggota",
+  IP: "ip",
+  SIRKULASI: "sirkulasi",
+  SIRKULASI_JALAN: "sirkulasi_jalan",
+  SU: "su",
+  PD: "pd",
+  NAIK_TURUN: "naik_turun",
 } as const;
 
 export  {
